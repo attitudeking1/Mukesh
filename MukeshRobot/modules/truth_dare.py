55815584362baa1cf5c7a93b80c2b89f42b81689
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2024-02-08 05:47:35 +0530

    Update truth_dare.py

--- MukeshRobot/modules/truth_dare.py
+++ MukeshRobot/modules/truth_dare.py
@@ -7,12 +7,12 @@ from MukeshRobot.modules.disable import DisableAbleCommandHandler
 
 
 def truth(update: Update, context: CallbackContext):
-    truth = requests.get(f"https://api.truthordarebot.xyz/v1/truth").json()["question"]
+    truth = requests.get("https://mukesh-api.vercel.app/truth").json()["results"]
     update.effective_message.reply_text(truth)
 
 
 def dare(update: Update, context: CallbackContext):
-    dare = requests.get(f"https://api.truthordarebot.xyz/v1/dare").json()["question"]
+    dare = requests.get("https://mukesh-api.vercel.app/dare").json()["results"]
     update.effective_message.reply_text(dare)
 
 TRUTH_HANDLER = DisableAbleCommandHandler("truth", truth, run_async=True)
@@ -22,8 +22,8 @@ dispatcher.add_handler(TRUTH_HANDLER)
 dispatcher.add_handler(DARE_HANDLER)
 
 __help__ = """
-*ᴛʀᴜᴛʜ & ᴅᴀʀᴇ*
- ❍ /truth  *:* sᴇɴᴅs ᴀ ʀᴀɴᴅᴏᴍ ᴛʀᴜᴛʜ sᴛʀɪɴɢ.
- ❍ /dare  *:* sᴇɴᴅs ᴀ ʀᴀɴᴅᴏᴍ ᴅᴀʀᴇ sᴛʀɪɴɢ.
+*Truth & dare*
+ ❂ /truth  *:* send a random truth string.
+ ❂ /dare  *:* send a random dare string.
 """
-__mod_name__ = "Tʀᴜᴛʜ-Dᴀʀᴇ"
+__mod_name__ = "Truth-dare"
