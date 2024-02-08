ade231ccb17e044ab69119667eafc2eec60b82c7
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2024-02-08 05:52:53 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -7,9 +7,9 @@ class Config(object):
     API_ID = "" # integer value, dont use ""
     API_HASH = ""
     TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 2145093972 # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_ID = 5072409665 # If you dont know, run the bot and do /id in your private chat with it, also an integer
     
-    SUPPORT_CHAT = "the_support_chat"  # Your own group for support, do not add the @
+    SUPPORT_CHAT = "chatsHouse"  # Your own group for support, do not add the @
     START_IMG = ""
     EVENT_LOGS = ()  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
     MONGO_DB_URI= ""
@@ -19,7 +19,10 @@ class Config(object):
         ""  # Get your API key from https://www.alphavantage.co/support/#api-key
     )
     TIME_API_KEY = ""
+    
     # Get your API key from https://timezonedb.com/api
+    API_KEY=""
+    #generate api from telegram using /token command bot username :>> @adventure_robot
 
     # Optional fields
     BL_CHATS = []  # List of groups that you want blacklisted.
@@ -36,7 +39,7 @@ class Config(object):
     LOAD = []
     NO_LOAD = []
     STRICT_GBAN = True
-    TEMP_DOWNLOAD_DIRECTORY = "./"
+    TEMP_DOWNLOAD_DIRECTORY = "./!"
     WORKERS = 8
     
 
